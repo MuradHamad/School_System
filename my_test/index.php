4ea00089d238b63e7776d7b118e09973a2ed1c6a
Layth45 <119439975+Layth45@users.noreply.github.com>
2025-01-04 16:47:27 +0300

    Update index.php

--- my_test/index.php
+++ my_test/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Constants in CSS</title>
-    <link rel="stylesheet" href="style.php"> <!-- Linking to PHP generated CSS -->
+    <link rel="stylesheet" href="style.php"> 
 </head>
 
 <body>
